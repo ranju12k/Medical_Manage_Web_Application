0b482e432d22579dba9e1e2a0aff28227aa3b69c
Ranjith M R <Ranjith.MR@sonata-software.com>
2025-03-24 12:45:19 +0530

    added ui components

---
+++ front_end/admin/doc-login/doc-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doc-login',
+  imports: [],
+  templateUrl: './doc-login.component.html',
+  styleUrl: './doc-login.component.scss'
+})
+export class DocLoginComponent {
+
+}
